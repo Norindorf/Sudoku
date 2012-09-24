a6c51d582c7ebb6e1b0f9b50d10c6a8e169050f4
Dmytro Sakhno <norindorf@gmail.com>
2012-09-24 17:37:20 +0300

    Project created
    Thank's to @tibidoh for Field.java template

---
+++ src/me/norindorf/sudoku/Field.java
@@ -0,0 +1,97 @@
+package me.norindorf.sudoku;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Field {
+    /**
+     * Забирает значение в ячейке
+     * @param x
+     * @param y
+     * @throws IndexOutOfBoundsException если координаты вылазят за поле
+     */
+    public int getValue( int x, int y ) throws IndexOutOfBoundsException {
+        //TODO
+        return 0;
+    }
+
+    /**
+     * Устанавливает значение в ячейке
+     * @param x
+     * @param y
+     * @param value
+     * @throws IndexOutOfBoundsException если координаты вылазят за поле
+     * @throws IllegalArgumentException если значение не приемлимо (0>val || 9<val)
+     *
+     */
+    public void setValue( int x, int y, int value ) throws IndexOutOfBoundsException, IllegalArgumentException {
+        //TODO
+    }
+
+    /**
+     * Забирает список заметок, сделанных пользователем
+     * @param x
+     * @param y
+     * @return список заметок на ячейке
+     * @throws IndexOutOfBoundsException если координаты вылазят за поле
+     */
+    public Set<Integer> getHints( int x, int y ) throws IndexOutOfBoundsException {
+        //TODO
+        return new HashSet<Integer>();
+    }
+
+
+    /**
+     * Устанавливает список заметок, сделанных пользователем
+     * @param x
+     * @param y
+     * @param hints список заметок о поле (набор цифр от 1 до 9)
+     * @throws IndexOutOfBoundsException если координаты вылазят за поле
+     * @throws IllegalArgumentException если значение не приемлимо (0>val || 9<val)
+     */
+    public void setHints( int x, int y, Set<Integer> hints ) throws IndexOutOfBoundsException, IllegalArgumentException {
+        //TODO
+    }
+
+    /**
+     * Добавляет одну заметку в ячейку
+     * @param x
+     * @param y
+     * @param hint
+     * @throws IndexOutOfBoundsException
+     * @throws IllegalArgumentException
+     */
+    public void addHint( int x, int y, int hint ) throws IndexOutOfBoundsException, IllegalArgumentException {
+        //TODO
+    }
+
+    /**
+     * Убирает одну заметку из ячейки
+     * @param x
+     * @param y
+     * @param hint
+     * @throws IndexOutOfBoundsException
+     * @throws IllegalArgumentException
+     */
+    public void removeHint( int x, int y, int hint ) throws IndexOutOfBoundsException, IllegalArgumentException {
+        //TODO
+    }
+
+    /**
+     * Проверяет, нет ли ошибок на поле
+     * @return true если ошибок нет
+     */
+    public boolean checkValid(){
+        //TODO
+        return false;
+    }
+
+    /**
+     * Проверяет, решено ли поле
+     * @return true  если решено
+     */
+    public boolean checkSolved(){
+        //TODO
+        return false;
+    }
+}
---
+++ src/me/norindorf/sudoku/Main.java
@@ -0,0 +1,5 @@
+package me.norindorf.sudoku;
+
+public class Main {
+
+}
